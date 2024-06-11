0083d7d2d0e494d325aac78c5c882e3c74a66e32
Vamshi Durganala <vdurganala@TGS.local>
2024-06-11 12:24:51 -0400

    updated

--- src/PageCounter.js
+++ src/PageCounter.js
@@ -1,12 +1,26 @@
-import React from 'react'
+import React, { useEffect } from 'react';
 
-function PageCounter() {
+const PageCounter = () => {
+  useEffect(() => {
+    const script = document.createElement('script');
+    script.src = 'https://counter4.optistats.ovh/private/counter.js?c=1q89nrwgn5dgrclf7hwlg5mnzcdwey9m&down=async';
+    script.async = true;
+    document.getElementById('web-counter').appendChild(script);
+    
+    return () => {
+      document.getElementById('web-counter').removeChild(script);
+    };
+  }, []);
+
   return (
-    <div>
-       <a href='http://www.freevisitorcounters.com'>at www.freevisitorcounters.com</a> <script type='text/javascript' src='https://www.freevisitorcounters.com/auth.php?id=138259fca7a532c845b970c2c63f0bbe28fa41ea'></script>
-       <script type="text/javascript" src="https://www.freevisitorcounters.com/en/home/counter/1191461/t/2"></script>
+    <div id="web-counter">
+      <noscript>
+        <a href="https://www.freecounterstat.com" title="free web counter html">
+          <img src="https://counter4.optistats.ovh/private/freecounterstat.php?c=1q89nrwgn5dgrclf7hwlg5mnzcdwey9m" border="0" title="free web counter html" alt="free web counter html" />
+        </a>
+      </noscript>
     </div>
-  )
-}
+  );
+};
 
-export default PageCounter
+export default PageCounter;
